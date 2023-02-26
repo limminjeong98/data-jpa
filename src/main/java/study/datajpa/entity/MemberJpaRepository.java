b8e673aeeb088225bf5e54b5936d212eb097b2a2
limminjeong <limminjeong98@naver.com>
2023-02-26 10:29:05 +0900

    build.gradle 수정

---
+++ src/main/java/study/datajpa/entity/MemberJpaRepository.java
@@ -0,0 +1,21 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    EntityManager em;
+
+    public Member save(Member member) {
+        em.persist(member);
+        return member;
+    }
+
+    public Member find(Long id) {
+        return em.find(Member.class, id);
+    }
+}
